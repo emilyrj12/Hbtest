998c9ce9d8dec2d9e3a8da30295adf11844f1559
emilyrj12 <emilyjose1597@gmail.com>
2023-06-02 12:39:03 +0100

    Add new feature

--- index.ts
+++ index.ts
@@ -1,3 +1,27 @@
+import * as express from 'express';
+import { Request, Response }  from 'express';
 import { sortProducts } from './src/controllers/productController';
 
-sortProducts();
+const app = express();
+const port = 3000;
+
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Please go to /sort!')
+})
+
+app.get('/sort', async (req: Request, res: Response) => {
+  try {
+    var sorted = await sortProducts();
+    
+    res.sendFile(sorted, () => {})
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred.');
+  }
+});
+
+app.listen(3000, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+   
